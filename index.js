9691c0d745e6c67b73cdd539d3be05e0c8264b68
Soudip Panja <soudip03panja@gmail.com>
2025-07-02 17:23:29 +0530

    Fix: Fixed get rout.

--- index.js
+++ index.js
@@ -44,10 +44,10 @@ async function seedData() {
 }
 // seedData();
 
-
 async function readAllJobs() {
   try {
     const allJobs = await Job.find();
+    console.log(allJobs);
     return allJobs;
   } catch (error) {
     console.log("Error reading jobs:", error.message);
@@ -55,15 +55,16 @@ async function readAllJobs() {
   }
 }
 
-
 app.get("/jobs", async (req, res) => {
   try {
     const jobs = await readAllJobs();
-    if (jobs.length === 0) {
-      return res.status(404).json({ error: "No jobs found." });
+    if (jobs && jobs.length > 0) {
+      res.status(200).json({ jobs });
+    } else {
+      res.status(404).json({ error: "No jobs found." });
     }
-    res.status(200).json({ jobs });
   } catch (error) {
+    console.error("Error fetching jobs:", error);
     res.status(500).json({ error: "Failed to fetch jobs." });
   }
 });
@@ -81,8 +82,7 @@ app.get("/jobs/:id", async (req, res) => {
   }
 });
 
-
 const PORT = process.env.PORT || 3000;
 app.listen(PORT, () => {
-  console.log(`✅ Server is running on port ${PORT}`);
+  console.log(`Server is running on port ${PORT}`);
 });
